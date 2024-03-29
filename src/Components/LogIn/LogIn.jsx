41817d5c078f7a70267f1aa3e80d6672b6e96622
OHshajim <ajshajimmax@gmail.com>
2024-03-30 00:28:13 +0600

    google provider add

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,5 +1,10 @@
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import app from "../../FireBase/Firebase";
 
 const LogIn = () => {
+    const auth = getAuth(app);
+    const provider = new GoogleAuthProvider();
+
     return (
         <div>
             
