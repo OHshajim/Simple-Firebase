0d4a21333489267bd2a94d5dcb0396900f399dc9
OHshajim <ajshajimmax@gmail.com>
2024-03-30 02:15:39 +0600

    google login

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,13 +1,24 @@
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import app from "../../FireBase/Firebase";
 
 const LogIn = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth,provider)
+        .then((result)=>{
+            const user = result.user ;
+            console.log(user);
+        })
+        .catch(error=>{
+            console.log(error.message);
+        })
+    }
+
     return (
-        <div>
-            
+        <div className="text-center">
+            <button onClick={handleGoogleLogin} className="btn">Google log in</button>
         </div>
     );
 };
