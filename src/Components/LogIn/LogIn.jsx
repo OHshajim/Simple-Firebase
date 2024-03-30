1849e47e5bccf07531bd33bc622740ac5bd069f5
OHshajim <ajshajimmax@gmail.com>
2024-03-30 13:05:54 +0600

    github log in

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,13 +1,14 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../FireBase/Firebase";
 import { useState } from "react";
 
 const LogIn = () => {
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const GoogleProvider = new GoogleAuthProvider();
+    const GitProvider = new GithubAuthProvider();
     const [user, setUser] = useState(null)
     const handleGoogleLogin = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, GoogleProvider)
             .then((result) => {
                 const logInUser = result.user;
                 console.log(logInUser);
@@ -16,22 +17,41 @@ const LogIn = () => {
             .catch(error => {
                 console.log(error.message);
             })
-    }
-    const handleLogout=()=>{
+        }
+
+        // for github
+        const GithubLogin = () => {
+            signInWithPopup(auth, GitProvider)
+                .then((result) => {
+                    const logInUser = result.user;
+                    console.log(logInUser);
+                    setUser(logInUser)
+                })
+                .catch(error => {
+                    console.log(error.message);
+                })
+        }
+        
+    const handleLogout = () => {
         signOut(auth)
-        .then(result=>{
-            console.log(result);
-            setUser(null)
-        })
-        .catch(error=>{
-            console.log(error);
-        })
+            .then(result => {
+                console.log(result);
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
+
 
     return (
         <div className=" flex flex-col justify-center items-center ">
-           { !user? <button onClick={handleGoogleLogin} className="btn">log in</button>:
-            <button onClick={handleLogout} className="btn"> log out</button>}
+            {!user ?
+                <>
+                    <button onClick={handleGoogleLogin} className="btn">Google log in</button>
+                    <button onClick={GithubLogin} className="btn">Github log in</button>
+                </> :
+                <button onClick={handleLogout} className="btn"> log out</button>}
             {user && <div>
                 <img src={user.photoURL} alt="user" />
                 <h2>Name : {user.displayName}</h2>
